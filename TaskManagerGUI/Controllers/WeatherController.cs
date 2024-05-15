30b790ce519d13f90096c7c15b6f201c1d1d4b2f
mmartin46 <mmartin46@angelo.edu>
2024-05-15 10:13:46 -0500

    Added changes

---
+++ TaskManagerGUI/Controllers/WeatherController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagerGUI.Controllers
+{
+    public class WeatherController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
