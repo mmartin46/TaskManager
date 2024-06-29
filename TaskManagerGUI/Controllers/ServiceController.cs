5e34b7222238a0f1fa841bc0945977863f2de78e
mmartin46 <mmartin46@angelo.edu>
2024-06-29 13:55:58 -0500

    changes made

--- TaskManagerGUI/Controllers/ServiceController.cs
+++ TaskManagerGUI/Controllers/ServiceController.cs
@@ -7,7 +7,7 @@ namespace TaskManagerGUI.Controllers
 {
     public class ServiceController : Controller
     {
-        private readonly IServiceRepository _serviceRepository = null;
+        private readonly IServiceRepository? _serviceRepository = null;
 
         public ServiceController(IServiceRepository serviceRepository)
         {
@@ -16,26 +16,30 @@ namespace TaskManagerGUI.Controllers
 
         public async Task<ViewResult> Index([FromQuery] string? name)
         {
-            var allServices = await _serviceRepository.GetServices(name);
+            var allServices = new List<ServiceModel>();
+            if (_serviceRepository is not null)
+            {
+                allServices = await _serviceRepository.GetServices(name);
+            }
             return View(allServices.Take(100).ToList());
         }
         [HttpPost]
         public async Task<IActionResult> ConfigureService(string displayName, string status)
         {
-
-            await _serviceRepository.ConfigureService(displayName, status);
+            if (_serviceRepository is not null)
+            {
+                await _serviceRepository.ConfigureService(displayName, status);
+            }
             return RedirectToAction(nameof(Index));
         }
 
 
 
         [HttpPost]
-        public async Task<IActionResult> QueryService([FromBody] ServiceModel service)
+        public IActionResult QueryService([FromBody] ServiceModel service)
         {
-            var services = await _serviceRepository.GetServices(service.DisplayName);
-
             var url = Url.Action("Index", "Service", new { name = service.DisplayName });
-            return Redirect(url);
+            return Redirect(url ?? nameof(Index));
         }
     }
 }
